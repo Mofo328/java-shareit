d8c77aa6010f144da42f736797d1d96aeaf6bc98
Mofo328 <163290172+Mofo328@users.noreply.github.com>
2024-09-27 12:39:54 +0500

    Небольшая корректировка кода

--- src/main/java/ru/practicum/shareit/error/RequestConflictException.java
+++ src/main/java/ru/practicum/shareit/error/RequestConflictException.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.error;
 
-public class RequestConflictException extends RuntimeException{
+public class RequestConflictException extends RuntimeException {
     public RequestConflictException(String message) {
         super(message);
     }
